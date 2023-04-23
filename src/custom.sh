f6ad9d7ac0c0e1da806c9264993dc3111c4b1372
queil <4584075+queil@users.noreply.github.com>
2023-04-23 21:34:29 +0200

    rooz: specify global cache for podman

--- src/custom.sh
+++ src/custom.sh
@@ -5,4 +5,5 @@ eval "$(starship init bash)"
 export ROOZ_USER=queil
 export ROOZ_IMAGE=ghcr.io/queil/image:latest
 export ROOZ_SHELL=bash
+export ROOZ_CACHES='~/.local/share/containers/storage/'
 export DOCKER_HOST=unix:///run/user/1000/podman/podman.sock
