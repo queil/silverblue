b07d67087f57bf0c0de61f53c04160f47e380a6f
queil <queil@users.noreply.github.com>
2023-12-27 15:21:58 +0000

    fix: tailcale & rider

--- src/init.sh
+++ src/init.sh
@@ -1,4 +1,5 @@
 dconf load / < /etc/settings.dconf
 systemctl --user enable --now podman.socket
+systemctl enable --now tailscaled
 podman pull ghcr.io/queil/image:latest
 cp /etc/vscode.json ~/.config/Code/User/settings.json
