9b10bce59c7b8be67791bd442e721f3cbae03bf8
queil <queil@users.noreply.github.com>
2023-12-28 10:32:02 +0000

    fix: refac settings

--- src/etc/custom/custom.sh
+++ src/etc/custom/custom.sh
@@ -10,4 +10,3 @@ export DOCKER_HOST=unix:///run/user/1000/podman/podman.sock
 
 alias nano=micro
 alias nano2=nano
-
--- src/etc/custom/init.sh
+++ src/etc/custom/init.sh
@@ -1,5 +1,5 @@
-dconf load / < /etc/settings.dconf
+dconf load / < /etc/custom/settings.dconf
 systemctl --user enable --now podman.socket
 systemctl enable --now tailscaled
 podman pull ghcr.io/queil/image:latest
-cp /etc/vscode.json ~/.config/Code/User/settings.json
+cp /etc/custom/.config/* -R  ~/.config/
